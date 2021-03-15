501b77c00a09036c3180df8c857fe875af6ef488
admarton <n4yhn4@inf.elte.hu>
2021-03-15 10:51:01 +0100

    Added getters and implementations to QuizItem

--- src/model/quizitem.h
+++ src/model/quizitem.h
@@ -9,12 +9,23 @@ class QuizItem
 public:
     QuizItem(std::string question, std::string right_answer, std::string wrong_answer_1, std::string wrong_answer_2, std::string wrong_answer_3, int trys = 0, int correct = 0)
         : qiuz_item_{ question, right_answer, wrong_answer_1, wrong_answer_2, wrong_answer_3 }, stat_{ trys, correct} {}
-    std::string getQuestion();
-    std::string getRightAnswer();
-    std::string getWrongAnswer1();
-    std::string getWrongAnswer2();
-    std::string getWrongAnswer3();
+    std::string getQuestion() const;
+    std::string getRightAnswer() const;
+    std::string getWrongAnswer1() const;
+    std::string getWrongAnswer2() const;
+    std::string getWrongAnswer3() const;
+    int getTrys() const;
+    int getCorrects() const;
+
     void newTry(bool correct);
+
+    void setQuestion(std::string question);
+    void setRightAnswer(std::string right_answer);
+    void setWrongAnswer1(std::string wrong_answer_1);
+    void setWrongAnswer2(std::string wrong_answer_2);
+    void setWrongAnswer3(std::string wrong_answer_3);
+    void setTrys(int trys);
+    void setCorrects(int corrects);
 
     friend std::ostream& operator<<(std::ostream& os, const QuizItem& q);
     friend std::istream& operator>>(std::istream& is, QuizItem& q);
