c64c7a4cb2f9c0844da85cc4dc979d1078ba0335
Ambrus-Dobai Márton <n4yhn4@inf.elte.hu>
2021-03-17 19:27:53 +0100

    Model class created

---
+++ src/model/model.h
@@ -0,0 +1,21 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "quizitem.h"
+
+#include <QVector>
+
+
+
+class Model
+{
+public:
+    Model();
+    QVector<QuizItem> active_quiz;
+
+
+private:
+
+};
+
+#endif // MODEL_H
