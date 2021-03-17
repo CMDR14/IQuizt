a1a9b99528173b40732aa08d222eb6e31c71019a
Ambrus-Dobai Márton <n4yhn4@inf.elte.hu>
2021-03-17 19:42:47 +0100

    Added ubuntu build script with cmake project.

---
+++ ubuntu_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dir="build"
+project_dir="IQuizt"
+
+
+#delete, create and move to dir
+cd ..
+if [ -d "$dir" ] eq false; then
+#    rm -rf $dir
+    mkdir $dir
+fi
+cd $dir
+
+#install packages
+sudo apt install -y cmake
+sudo apt install -y make
+sudo apt install -y qt5-default 
+
+#build project
+cmake ../$project_dir
+cmake --build . --
+
+#run the app
+./$project_dir
+
