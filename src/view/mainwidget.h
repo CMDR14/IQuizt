29b9bd6a8efe639e48e3080915bc45977d498a7f
Berta Gábor Pál <eoxdzj@inf.elte.hu>
2021-04-02 21:27:24 +0200

    Tests Created and modified Created Persistence tests and deleted bad, prevous tests. Modified some header includes to work with.

--- src/view/mainwidget.h
+++ src/view/mainwidget.h
@@ -2,10 +2,10 @@
 #define MAINWIDGET_H
 
 #include "../model/model.h"
+#include <QWidget>
 #include <QListWidget>
 #include <QMenuBar>
 #include <QVBoxLayout>
-#include <QWidget>
 #include "quizitemselector.h"
 #include "../model/quizitem.h"
 
@@ -13,7 +13,7 @@ class MainWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit MainWidget(QWidget *parent = nullptr);
+    explicit MainWidget(Model *model, QWidget *parent = nullptr);
 
 
 private:
--- tests/PersistenceTest/tst_persistencetest.cpp
+++ tests/PersistenceTest/tst_persistencetest.cpp
@@ -1,19 +1,23 @@
 #include <QtTest>
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
 
 // add necessary includes here
-#include "../../src/persistence/persistence.cpp"
+#include "../../src/persistence/persistence.h"
 
 class PersistenceTest : public QObject
 {
     Q_OBJECT
-
 public:
     PersistenceTest();
     ~PersistenceTest();
     Persistence* persistence;
 
 private slots:
-    void test_case1();
+    void test_get_quiz_sets_for_null_file();
+    void test_get_quiz_sets_for_existing_file();
+    void test_wrong_file_name();
 
 };
 
@@ -27,10 +31,60 @@ PersistenceTest::~PersistenceTest()
 
 }
 
-void PersistenceTest::test_case1()
+void PersistenceTest::test_get_quiz_sets_for_null_file()
 {
-    QCOMPARE(1,1);
+    QVector<NameAndPath> sets;
+
+    persistence->get_quiz_sets(sets);
+
+    QCOMPARE(sets.count(),0);
 }
+
+void PersistenceTest::test_get_quiz_sets_for_existing_file()
+{
+    QFile file(QDir::currentPath() + "/test.quiz");
+    if(!file.open(QFile::WriteOnly))
+            QCOMPARE(1,0);
+
+    QTextStream stream(&file);
+
+    stream << "Test line writed" << "\n";
+
+    file.close();
+
+    QVector<NameAndPath> sets;
+    persistence->get_quiz_sets(sets);
+
+    QCOMPARE(sets.count(),1);
+    QCOMPARE(sets.at(0).name, "Test line writed");
+    QCOMPARE(sets.at(0).path,"test.quiz");
+    file.remove();
+
+}
+
+
+
+void PersistenceTest::test_wrong_file_name()
+{
+    QFile file(QDir::currentPath() + "/test.text");
+    if(!file.open(QFile::WriteOnly))
+            QCOMPARE(1,0);
+
+    QTextStream stream(&file);
+
+    stream << "Test" << "\n";
+
+    file.close();
+
+    QVector<NameAndPath> sets;
+    persistence->get_quiz_sets(sets);
+
+
+    QCOMPARE(sets.count(),0);
+
+    file.remove();
+}
+
 
 QTEST_APPLESS_MAIN(PersistenceTest)
 
