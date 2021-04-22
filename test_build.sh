64a5b64319cb79349109e143a256ee868925c0fd
Ambrus-Dobai Márton <n4yhn4@inf.elte.hu>
2021-04-22 14:53:59 +0200

    Added comment to test_build script

--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Makes only the given test from the test folder
+#first parameter is the name of the directory in the test folder
+#second parameter is a optional -ctest flag, if given, ctest will run
+
 dir="build"
 project_dir=$1
 
